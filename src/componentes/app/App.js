9b23230bd6ffc2fcdb18f9c06c4f90a86238792c
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-06-19 17:45:54 -0300

    a primeira parte está finalizada

--- src/componentes/app/App.js
+++ src/componentes/app/App.js
@@ -1,6 +1,6 @@
 import styles from'./App.css';
 import { Route, Routes, BrowserRouter, NavLink } from 'react-router-dom';
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import Root from '../root/root';
 import Contact from "../contacts/contact";
 import Appointment from "../appointments/appointment";
@@ -11,55 +11,37 @@ import Appointment from "../appointments/appointment";
 
 function App() {
 
-  const [name, setName] = useState([{}]);
-  const [email, setEmail] = useState([{}]);
-  const [contato, setContato] = useState([{}]);
-  const [data, setData] = useState({
-      name: name,
-      email: email,
-      contato: contato
-  })
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [contato, setContato] = useState();
   const [divs, setDivs] = useState([]);
 
   
 
   const submitForm = (e) => {
     e.preventDefault();
-   // let content = {name, email, contato}
-  //  fetch('http://localhost:3000/Contact', {
-  //    method: 'POST',
- //     headers: {"Content-Type": "application/json"},
-   //   body: JSON.stringify(content)
-//    }).then(() => {
- //     console.log('hello, world');
- //   })
-   
+
     
+      setDivs([...divs, <div className='newDiv' key={divs.length}>
+        {name}<br></br> 
+        {email}<br></br>
+        {contato}{divs.length + 1} 
+        </div>]);    
     }
       
-      
-      
-   //useEffect(() => {
-  //    let change = window.localStorage.getItem('dados');
-   //   setData(JSON.parse(change))
- //  })
-  
-    useEffect(() => {
-      window.localStorage.setItem('dados', JSON.stringify(data));
-    }, [data])
+
+
     
-
-
-  
-  
 
   return (
     <BrowserRouter>
     <div className='appContainer'>
       <header>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to='Contact'>Contacts</NavLink>
-        <NavLink to='Appointment'>Appointments</NavLink>
+        <nav className='nav'>
+          <NavLink className='textNav' to='/'>Home</NavLink>
+          <NavLink className='textNav' to='Contact'>Contacts</NavLink>
+          <NavLink className='textNav' to='Appointment'>Appointments</NavLink>
+        </nav>
       </header>
       <main>
         <Routes>
@@ -75,10 +57,3 @@ function App() {
 
 export default App;
 
-//setName={setName} setEmail={setEmail} setContato={setContato} name={name}
-
-//setDivs([...divs, <div className='example' key={divs.length}>
- // {name}<br></br> 
- // {email}<br></br>
- //{contato}{divs.length + 1} 
- //</div>]);
--- src/componentes/contacts/contact.js
+++ src/componentes/contacts/contact.js
@@ -15,13 +15,13 @@ function Contact (props) {
             <section>
                 <form className={styles.formulario}>
                     <label for='nome'>Nome: </label>
-                    <input onChange={(e) => setName (e.target.value)} className={styles.formChild} id="nome" type="text"></input><br></br>
+                    <input required  placeholder="Name" onChange={(e) => setName (e.target.value)} className={styles.formChild} id="nome" type="text"></input><br></br>
 
                     <label for="email">Email: </label>
-                    <input onChange={(e) => setEmail (e.target.value)} className={styles.formChild} id='email'></input><br></br>
+                    <input required placeholder="Email" onChange={(e) => setEmail (e.target.value)} className={styles.formChild} id='email'></input><br></br>
 
                     <label for="contato">Contato: </label>
-                    <input onChange={(e) => setContato (e.target.value)} className={styles.formChild} id='contato' type="number"></input><br></br>
+                    <input required placeholder="Contact" onChange={(e) => setContato (e.target.value)} className={styles.formChild} id='contato'></input><br></br>
 
                     <button type="submit" onClick={props.submitForm} className={styles.button}>Save</button>
                 </form>
@@ -29,20 +29,8 @@ function Contact (props) {
 
             <section>
                 <h3>Lista de Contatos</h3>
-                <div className={styles.example}>
-                    <h5>Ana Gabriela</h5>
-                    <p>41281582810</p>
-                    <p>afnaflvd@gmail.com</p>
-                </div>
-                <div className={styles.example}>
-                    <h5>{props.name}</h5>
-                    <p></p>
-                    <p></p>
-                </div>
-
                 <div className={styles.newDiv}>
                     {props.divs}
-                    <button>Delete</button>
                 </div>
             </section>
         </div>
@@ -52,9 +40,3 @@ function Contact (props) {
 
 export default Contact;
 
-//<div className={styles.example}>
-//<h5>{props.name}</h5>
-//<p>{props.sobrenome}</p>
-//<p>{props.email}</p>
-//<p>{props.contato}</p>
-//</div>
