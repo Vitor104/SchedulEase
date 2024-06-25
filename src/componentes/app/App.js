163bb27b11d8f5db30eefa56b80b629a66519d66
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-06-25 18:56:02 -0300

    Update App.js

--- src/componentes/app/App.js
+++ src/componentes/app/App.js
@@ -11,9 +11,9 @@ import Appointment from "../appointments/appointment";
 
 function App() {
 
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [contato, setContato] = useState();
+  const [name, setName] = useState([]);
+  const [email, setEmail] = useState([]);
+  const [contato, setContato] = useState([]);
   const [description, setDescription] = useState();
   const [divs, setDivs] = useState([]);
   const [option, setOption] = useState([]);
@@ -21,25 +21,16 @@ function App() {
   const [date, setDate] = useState();
   const [time, setTime] = useState();
   const [title, setTitle] = useState();
-  const [teste, setTeste] = useState(
-    [{
-      name: name, email: email, contato: contato
-    }]);
+ console.log(divs);
   
  
   const submitForm = (e) => {
     e.preventDefault();
 
     
-        setDivs([...divs, 
-         <div className='newDiv' key={divs.length}>
-
-          {name}<br></br> 
-          {email}<br></br>
-          {contato}  
-          {divs.length + 1}   
-          
-        </div>])
+       setDivs([...divs, divs.map((name, email, contato, index) => {
+        <div key={index}>{name}</div>
+       })])
 
           setOption([...option, 
           <option className={styles.newOption} key={option.length}>
@@ -83,15 +74,13 @@ function App() {
         <Routes>
           <Route path='/' element={ <Root />} />
 
-          <Route path='Contact' element={ <Contact divs={divs} setName={setName} setEmail={setEmail} setContato={setContato} submitForm={submitForm}   />}/>
+          <Route path='Contact' element={ <Contact divs={divs} setName={setName} setEmail={setEmail} setContato={setContato} submitForm={submitForm} />}/>
 
-          <Route path='Appointment' element={ teste.map((exemplo) => {
-            console.log(exemplo);
-            return (
+          <Route path='Appointment' element={ 
               <Appointment 
-                name={exemplo.name}
-                email={exemplo.email}
-                contato={exemplo.contato}
+                name={name}
+                email={email}
+                contato={contato}
                 setDate={setDate} 
                 setTime={setTime} 
                 setTitle={setTitle}
@@ -100,8 +89,8 @@ function App() {
                 option={option} 
                 divsAptm={divsAptm}
               />
-            )
-          })} />
+            
+          } />
         </Routes>
       </main>
     </div>
